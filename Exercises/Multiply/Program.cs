191089ffd235d3160202d62299adc340c5ec3c4f
Luis Miguel Lopez Moreno <luiso.lo2704@gmail.com>
2025-03-16 17:30:58 -0500

    Save

--- Exercises/Multiply/Program.cs
+++ Exercises/Multiply/Program.cs
@@ -3,6 +3,89 @@ using Multiply;
 
 
 
-var a = ConsoleExtension.GetInt("This is the first line of code of this project");
+var answer = "";
+var options = new List<string> { "s", "n" };
 
-Console.WriteLine(a);
+do
+{
+
+
+    int m = ConsoleExtension.GetInt("Ingrese el valor de m:");
+    int n = ConsoleExtension.GetInt("Ingrese el valor de n:");
+    int p = ConsoleExtension.GetInt("Ingrese el valor de p:");
+
+    int[,] resultado = MultiplicarMatrices(m, n, p);
+
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Quieres continuar? (s/n): ", options);
+    } while (answer is null);
+
+} while (answer.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+static int[,] MultiplicarMatrices(int m, int n, int p)
+{
+    
+    int[,] A = new int[m, n];
+    int[,] B = new int[n, p];
+    int[,] C = new int[m, p];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            A[i, j] = (i + 1) * j;
+        }
+    }
+
+   
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < p; j++)
+        {
+            B[i, j] = (j + 1) * i;
+        }
+    }
+
+   
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < p; j++)
+        {
+            C[i, j] = 0;
+            for (int k = 0; k < n; k++)
+            {
+                C[i, j] += A[i, k] * B[k, j];
+            }
+        }
+    }
+
+   
+    Console.WriteLine("\n*** A ***");
+    ImprimirMatriz(A);
+
+    Console.WriteLine("\n*** B ***");
+    ImprimirMatriz(B);
+
+    Console.WriteLine("\n*** C ***");
+    ImprimirMatriz(C);
+
+    return C;
+}
+
+
+static void ImprimirMatriz(int[,] matriz)
+{
+    int filas = matriz.GetLength(0);
+    int columnas = matriz.GetLength(1);
+
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            Console.Write(matriz[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Exercises/Rhombuss/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Multiply;
+
+var answer = "";
+var options = new List<string> { "s", "n" };
+
+do
+{
+
+
+    int n = ConsoleExtension.GetInt("Ingrese el tamaño del rombo (impar): ");
+
+    if (n % 2 == 0)
+    {
+        Console.WriteLine("El tamaño debe ser un número impar.");
+        continue;
+    }
+
+
+    DibujarRombo(n);
+
+
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Quieres continuar? (s/n): ", options);
+    } while (answer is null);
+
+} while (answer.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+static void DibujarRombo(int n)
+{
+    char[,] rombo = new char[n, n];
+
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            rombo[i, j] = ' ';
+        }
+    }
+
+    int m = n / 2;
+
+    
+    for (int i = 0; i <= m; i++)
+    {
+        rombo[i, m - i] = '#'; 
+        rombo[i, m + i] = '#'; 
+        rombo[n - 1 - i, m - i] = '#'; 
+        rombo[n - 1 - i, m + i] = '#'; 
+    }
+
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(rombo[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
