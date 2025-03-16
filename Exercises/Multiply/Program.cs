7fb4af7665c5f98347a07cb27c403485c8155302
Luis Miguel Lopez Moreno <luiso.lo2704@gmail.com>
2025-03-16 15:39:23 -0500

    First Line

---
+++ Exercises/Multiply/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Multiply;
+
+
+
+var a = ConsoleExtension.GetInt("This is the first line of code of this project");
+
+Console.WriteLine(a);
